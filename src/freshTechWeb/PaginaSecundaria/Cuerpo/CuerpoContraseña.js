ebf7efb12456e3319a0dbe0adf5cb5536333f7f8
Diego García <767870@unizar.es>
2021-05-21 12:19:50 +0200

    falla-contra

--- src/freshTechWeb/PaginaSecundaria/Cuerpo/CuerpoContraseña.js
+++ src/freshTechWeb/PaginaSecundaria/Cuerpo/CuerpoContraseña.js
@@ -343,7 +343,7 @@ selectCategorias=async(value)=>{
 
 //CREA USER-PASSWD
   crearContrasenya=async(value)=>{
-    /*var catAEnviar=null;
+    var catAEnviar=null;
     if(localStorage.getItem('categoria') == null){
       if((this.miListaC.listaC[0] != "Sin categoría") && (this.miListaC.listaC[0] != "sin categorias disponibles")){
         catAEnviar = this.miListaC.listaC[0];
@@ -352,7 +352,7 @@ selectCategorias=async(value)=>{
     else if((localStorage.getItem('categoria') != "Sin categoría") && (localStorage.getItem('categoria') != "sin categorias disponibles")){
     
       catAEnviar=localStorage.getItem('categoria');
-    }*/
+    }
     const datos = {concreteuser:this.state.usuario,concretepasswd:this.state.contrasenya, dominio:this.state.url,fechacreacion:this.state.fecha_actual,fechacaducidad:this.state.fecha_caducidad,nombre:this.state.nombre,categoria:catAEnviar};
     const headers = {'Authorization':`Bearer ${value}`};
     await axios.post('https://fresh-techh.herokuapp.com/passwd',datos,{headers}
